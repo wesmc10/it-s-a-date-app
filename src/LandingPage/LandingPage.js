b764736e73282492ce20948e275e6c8814b245e9
Wes McElroy <mcelroywes@gmail.com>
2019-07-13 19:52:31 -0700

    Modified flex for LandingPage and LogIn components

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -12,23 +12,25 @@ export default function LandingPage(props) {
             <header className="Login_header">
                 <LogIn {...props} />
             </header>
-            <section className="Landing_page_content_section">
-                <h1 className="title">
-                    <span className="Calendar_icon">
-                        <FontAwesomeIcon
-                            icon={faCalendarAlt}
-                        />
-                    </span>
-                    It's a Date
-                </h1>
-                <h2 className="subtitle">Leave Your Worries Behind</h2>
-                <p className="paragraph_content">You've got enough on your mind, so relax and let 
-                It's a Date remember about all of your important events. Just create a calendar, 
-                input your event information, and never worry about forgetting events again.</p>
-            </section>
-            <section className="Signup_section">
-                <SignUp {...props} />
-            </section>
+            <div className="Landing_page_flex_container">
+                <section className="Landing_page_content_section">
+                    <h1 className="title">
+                        <span className="Calendar_icon">
+                            <FontAwesomeIcon
+                                icon={faCalendarAlt}
+                            />
+                        </span>
+                        It's a Date
+                    </h1>
+                    <h2 className="subtitle">Leave Your Worries Behind</h2>
+                    <p className="paragraph_content">You've got enough on your mind, so relax and let 
+                    It's a Date remember about all of your important events. Just create a calendar, 
+                    input your event information, and never worry about forgetting events again.</p>
+                </section>
+                <section className="Signup_section">
+                    <SignUp {...props} />
+                </section>
+            </div>
         </div>
     )
 }
