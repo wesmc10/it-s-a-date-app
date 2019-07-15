e5ebae623e84e1666117c128d7ca39ceab501ca1
Wes McElroy <mcelroywes@gmail.com>
2019-07-15 14:48:13 -0700

    Styled EditEvent component for larger screens

--- src/EditEvent/EditEvent.js
+++ src/EditEvent/EditEvent.js
@@ -128,90 +128,92 @@ export default class EditEvent extends Component {
                 <header className="Edit_event_header">
                     <LogOut />
                 </header>
-                <section>
-                    <h1 className="Edit_event_title">Edit Event</h1>
-                </section>
-                <section>
-                    <form className="Edit_event_form" onSubmit={this.handleSubmit}>
-                        <div role="alert">
-                            {error && <p className="red">{error}</p>}
-                        </div>
-                        <div className="Edit_event_form_name">
-                            <label htmlFor="Edit_event_name" className="Edit_event_name_label">
-                                Name
-                            </label>
-                            <input
-                                id="Edit_event_name"
-                                type="text"
-                                name="Edit_event_name"
-                                required
-                                value={name}
-                                onChange={this.handleNameChange}
-                            />
-                        </div>
-                        <div className="Edit_event_form_description">
-                            <label htmlFor="Edit_event_description" className="Edit_event_description_label">
-                                Description
-                            </label>
-                            <textarea
-                                id="Edit_event_description"
-                                name="Edit_event_description"
-                                rows='6'
-                                cols='40'
-                                required
-                                value={description}
-                                onChange={this.handleDescriptionChange}
-                            />
-                        </div>
-                        <div className="Edit_event_form_time">
-                            <label htmlFor="Edit_event_time" className="Edit_event_time_label">
-                                Time
-                            </label>
-                            <input
-                                id="Edit_event_time"
-                                type="time"
-                                name="Edit_event_time"
-                                required
-                                value={time}
-                                onChange={this.handleTimeChange}
-                            />
-                        </div>
-                        <div className="Edit_event_form_location">
-                            <label htmlFor="Edit_event_location" className="Edit_event_location_label">
-                                Location
-                            </label>
-                            <input
-                                id="Edit_event_location"
-                                type="text"
-                                name="Edit_event_location"
-                                required
-                                value={location}
-                                onChange={this.handleLocationChange}
-                            />
-                        </div>
-                        <div className="Edit_event_form_other">
-                            <label htmlFor="Edit_event_other" className="Edit_event_other_label">
-                                Other
-                            </label>
-                            <textarea
-                                id="Edit_event_other"
-                                name="Edit_event_other"
-                                rows='4'
-                                cols='40'
-                                value={other}
-                                onChange={this.handleOtherChange}
-                            />
-                        </div>
-                        <div className="Edit_event_buttons">
-                            <button type="submit" className="Edit_event_update">
-                                Update
-                            </button>
-                            <button type="reset" className="Edit_event_cancel" onClick={this.handleClickCancel}>
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                </section>
+                <div className="Edit_event_flex_container">
+                    <section>
+                        <h1 className="Edit_event_title">Edit Event</h1>
+                    </section>
+                    <section>
+                        <form className="Edit_event_form" onSubmit={this.handleSubmit}>
+                            <div role="alert">
+                                {error && <p className="red">{error}</p>}
+                            </div>
+                            <div className="Edit_event_form_name">
+                                <label htmlFor="Edit_event_name" className="Edit_event_name_label">
+                                    Name
+                                </label>
+                                <input
+                                    id="Edit_event_name"
+                                    type="text"
+                                    name="Edit_event_name"
+                                    required
+                                    value={name}
+                                    onChange={this.handleNameChange}
+                                />
+                            </div>
+                            <div className="Edit_event_form_description">
+                                <label htmlFor="Edit_event_description" className="Edit_event_description_label">
+                                    Description
+                                </label>
+                                <textarea
+                                    id="Edit_event_description"
+                                    name="Edit_event_description"
+                                    rows='6'
+                                    cols='40'
+                                    required
+                                    value={description}
+                                    onChange={this.handleDescriptionChange}
+                                />
+                            </div>
+                            <div className="Edit_event_form_time">
+                                <label htmlFor="Edit_event_time" className="Edit_event_time_label">
+                                    Time
+                                </label>
+                                <input
+                                    id="Edit_event_time"
+                                    type="time"
+                                    name="Edit_event_time"
+                                    required
+                                    value={time}
+                                    onChange={this.handleTimeChange}
+                                />
+                            </div>
+                            <div className="Edit_event_form_location">
+                                <label htmlFor="Edit_event_location" className="Edit_event_location_label">
+                                    Location
+                                </label>
+                                <input
+                                    id="Edit_event_location"
+                                    type="text"
+                                    name="Edit_event_location"
+                                    required
+                                    value={location}
+                                    onChange={this.handleLocationChange}
+                                />
+                            </div>
+                            <div className="Edit_event_form_other">
+                                <label htmlFor="Edit_event_other" className="Edit_event_other_label">
+                                    Other
+                                </label>
+                                <textarea
+                                    id="Edit_event_other"
+                                    name="Edit_event_other"
+                                    rows='4'
+                                    cols='40'
+                                    value={other}
+                                    onChange={this.handleOtherChange}
+                                />
+                            </div>
+                            <div className="Edit_event_buttons">
+                                <button type="submit" className="Edit_event_update">
+                                    Update
+                                </button>
+                                <button type="reset" className="Edit_event_cancel" onClick={this.handleClickCancel}>
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
             </div>
         )
     }
