ddace64864d13858efb7cd2c90dfde8fa38f7c3c
Wes McElroy <mcelroywes@gmail.com>
2019-07-14 21:31:25 -0700

    Began styling CreateEvent component for larger screens

--- src/CreateEvent/CreateEvent.js
+++ src/CreateEvent/CreateEvent.js
@@ -110,85 +110,87 @@ export default class CreateEvent extends Component {
                 <header className="Create_event_header">
                     <LogOut />
                 </header>
-                <section>
-                    <h1 className="Create_event_title">Create an Event</h1>
-                </section>
-                <section>
-                    <form className="Create_event_form" onSubmit={this.handleSubmit}>
-                        <div role="alert">
-                            {error && <p className="red">{error}</p>}
-                        </div>
-                        <div className="Create_event_form_name">
-                            <label htmlFor="Create_event_name" className="Create_event_name_label">
-                                Name
-                            </label>
-                            <input
-                                id="Create_event_name"
-                                type="text"
-                                name="Create_event_name"
-                                required
-                                onChange={this.handleNameChange}
-                            />
-                        </div>
-                        <div className="Create_event_form_description">
-                            <label htmlFor="Create_event_description" className="Create_event_description_label">
-                                Description
-                            </label>
-                            <textarea
-                                id="Create_event_description"
-                                name="Create_event_description"
-                                rows='6'
-                                cols='40'
-                                required
-                                onChange={this.handleDescriptionChange}
-                            />
-                        </div>
-                        <div className="Create_event_form_time">
-                            <label htmlFor="Create_event_time" className="Create_event_time_label">
-                                Time
-                            </label>
-                            <input
-                                id="Create_event_time"
-                                type="time"
-                                name="Create_event_time"
-                                required
-                                onChange={this.handleTimeChange}
-                            />
-                        </div>
-                        <div className="Create_event_form_location">
-                            <label htmlFor="Create_event_location" className="Create_event_location_label">
-                                Location
-                            </label>
-                            <input
-                                id="Create_event_location"
-                                type="text"
-                                name="Create_event_location"
-                                required
-                                onChange={this.handleLocationChange}
-                            />
-                        </div>
-                        <div className="Create_event_form_other">
-                            <label htmlFor="Create_event_other" className="Create_event_other_label">
-                                Other
-                            </label>
-                            <textarea
-                                id="Create_event_other"
-                                name="Create_event_other"
-                                rows='4'
-                                cols='40'
-                                onChange={this.handleOtherChange}
-                            />
-                        </div>
-                        <div className="Create_event_buttons">
-                            <button type="submit" className="Create_event_submit">
-                                Create
-                            </button>
-                            <button type="reset" className="Create_event_cancel" onClick={this.handleClickCancel}>
-                                Cancel
-                            </button>
-                        </div>
-                    </form>
-                </section>
+                <div className="Create_event_flex_container">
+                    <section>
+                        <h1 className="Create_event_title">Create an Event</h1>
+                    </section>
+                    <section>
+                        <form className="Create_event_form" onSubmit={this.handleSubmit}>
+                            <div role="alert">
+                                {error && <p className="red">{error}</p>}
+                            </div>
+                            <div className="Create_event_form_name">
+                                <label htmlFor="Create_event_name" className="Create_event_name_label">
+                                    Name
+                                </label>
+                                <input
+                                    id="Create_event_name"
+                                    type="text"
+                                    name="Create_event_name"
+                                    required
+                                    onChange={this.handleNameChange}
+                                />
+                            </div>
+                            <div className="Create_event_form_description">
+                                <label htmlFor="Create_event_description" className="Create_event_description_label">
+                                    Description
+                                </label>
+                                <textarea
+                                    id="Create_event_description"
+                                    name="Create_event_description"
+                                    rows='6'
+                                    cols='40'
+                                    required
+                                    onChange={this.handleDescriptionChange}
+                                />
+                            </div>
+                            <div className="Create_event_form_time">
+                                <label htmlFor="Create_event_time" className="Create_event_time_label">
+                                    Time
+                                </label>
+                                <input
+                                    id="Create_event_time"
+                                    type="time"
+                                    name="Create_event_time"
+                                    required
+                                    onChange={this.handleTimeChange}
+                                />
+                            </div>
+                            <div className="Create_event_form_location">
+                                <label htmlFor="Create_event_location" className="Create_event_location_label">
+                                    Location
+                                </label>
+                                <input
+                                    id="Create_event_location"
+                                    type="text"
+                                    name="Create_event_location"
+                                    required
+                                    onChange={this.handleLocationChange}
+                                />
+                            </div>
+                            <div className="Create_event_form_other">
+                                <label htmlFor="Create_event_other" className="Create_event_other_label">
+                                    Other
+                                </label>
+                                <textarea
+                                    id="Create_event_other"
+                                    name="Create_event_other"
+                                    rows='4'
+                                    cols='40'
+                                    onChange={this.handleOtherChange}
+                                />
+                            </div>
+                            <div className="Create_event_buttons">
+                                <button type="submit" className="Create_event_submit">
+                                    Create
+                                </button>
+                                <button type="reset" className="Create_event_cancel" onClick={this.handleClickCancel}>
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </section>
+                </div>
             </div>
         )
     }
